e7cd69d11f5de3c0973a6765d757f93a4ee2e824
StrenchTheBear <acliv2904@yahoo.com>
2021-05-31 16:53:48 -0500

    models

---
+++ AspNetCoreWebApplication/Models/ReadDB.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace Temp.Models
+{
+    public class ReadDB
+    {
+        
+        public string Fecha { get; set; }
+        public int Temperatura { get; set; }
+    }
+}
