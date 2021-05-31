ebcd6379151b85f0c406bf20029ea0531a1e68c3
StrenchTheBear <acliv2904@yahoo.com>
2021-05-31 17:31:18 -0500

    cambios

--- AspNetCoreWebApplication/Controllers/HomeController.cs
+++ AspNetCoreWebApplication/Controllers/HomeController.cs
@@ -2,17 +2,26 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Temp.Models;
 
 namespace AspNetCoreWebApplication.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
             ViewData["Message"] = "You just created a ASP.Net Core web application!";
-            return View();
+            var httpClient = new HttpClient();
+            var json = await httpClient.GetStringAsync("https://jgbo9n6qqk.execute-api.us-east-2.amazonaws.com/Testget/getreaddb");
+
+            var DBlist = JsonConvert.DeserializeObject<List<ReadDB>>(json); 
+           
+         
+            return View(DBlist);
         }
 
         public IActionResult Error()
